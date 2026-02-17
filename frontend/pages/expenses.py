2155980b7d1997c02501dc75c4ae77f05683b8bc
G-Sahil123 <150524506+G-Sahil123@users.noreply.github.com>
2026-02-17 16:51:59 +0530

    Create expenses.py

---
+++ frontend/pages/expenses.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from api import get, post
+
+group_id = st.session_state.get("group_id")
+
+st.title("💸 Expenses")
+
+res = get(f"/participants/{group_id}")
+participants = res.json()
+
+payer = st.selectbox(
+    "Paid By",
+    participants,
+    format_func=lambda x: x["name"]
+)
+
+amount = st.number_input("Amount", min_value=0.0)
+desc = st.text_input("Description")
+
+if st.button("Add Expense"):
+    post("/expenses", {
+        "groupId": group_id,
+        "description": desc,
+        "amount": amount,
+        "paidBy": payer["_id"],
+        "splitMode": "equal"
+    })
+    st.success("Expense added")
+    st.rerun()
