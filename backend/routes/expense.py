16192b86c9cf6240e935b0b849e2afb8ca370912
G-Sahil123 <150524506+G-Sahil123@users.noreply.github.com>
2026-02-17 16:26:27 +0530

    Update expense.py

--- backend/routes/expense.py
+++ backend/routes/expense.py
@@ -2,14 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.controllers.expense import add_expense, get_expenses
-from app.auth import get_current_user
+from app.core.auth import get_current_user
 
-router = APIRouter(prefix="/expenses", tags=["Expenses"])
+router = APIRouter(
+    prefix="/expenses",
+    tags=["Expenses"],
+    dependencies=[Depends(get_current_user)]
+)
+
 
 @router.post("/")
 def create_expense(
     payload: dict,
-    user=Depends(get_current_user),
     db: Session = Depends(get_db)
 ):
     return add_expense(payload, db)
@@ -18,7 +22,6 @@ def create_expense(
 @router.get("/{group_id}")
 def list_expenses(
     group_id: int,
-    user=Depends(get_current_user),
     db: Session = Depends(get_db)
 ):
     return get_expenses(group_id, db)
