265a1967639ea882dc910fbdff9d0ef6004dd241
G-Sahil123 <150524506+G-Sahil123@users.noreply.github.com>
2026-02-17 16:41:52 +0530

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,15 +1,14 @@
-import os
-import jwt
+# app/auth.py
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from dotenv import load_dotenv
-
-load_dotenv()
-
-JWT_SECRET = os.getenv("JWT_SECRET")
-ALGORITHM = "HS256"
+import jwt
+import os
 
 security = HTTPBearer()
+
+JWT_SECRET = os.getenv("JWT_SECRET", "supersecret")
+JWT_ALGORITHM = "HS256"
+
 
 def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security)
@@ -17,8 +16,8 @@ def get_current_user(
     token = credentials.credentials
 
     try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
-        return payload   # equivalent to req.user
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return payload   # this is like req.user
     except jwt.ExpiredSignatureError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
