379d8b94877938f2fa6f1874770759a58c9f3f69
G-Sahil123 <150524506+G-Sahil123@users.noreply.github.com>
2026-02-17 16:26:01 +0530

    Update balance.py

--- backend/routes/balance.py
+++ backend/routes/balance.py
@@ -2,14 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.controllers.balance import get_balances
-from app.auth import get_current_user
+from app.core.auth import get_current_user
 
-router = APIRouter(prefix="/balances", tags=["Balances"])
+router = APIRouter(
+    prefix="/balances",
+    tags=["Balances"],
+    dependencies=[Depends(get_current_user)]
+)
+
 
 @router.get("/analytics/{group_id}")
 def balances(
     group_id: int,
-    user=Depends(get_current_user),
     db: Session = Depends(get_db)
 ):
     return get_balances(group_id, db)
