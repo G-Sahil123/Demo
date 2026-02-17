60768c2100101760e1db4cddd3df3f7674a15a0d
G-Sahil123 <150524506+G-Sahil123@users.noreply.github.com>
2026-02-17 16:51:16 +0530

    Create groups.py

---
+++ frontend/pages/groups.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from api import get, post
+
+st.title("👥 Groups")
+
+# Create group
+name = st.text_input("Group Name")
+if st.button("Create Group"):
+    post("/groups", {"name": name})
+    st.success("Group created")
+    st.rerun()
+
+# List groups
+res = get("/groups")
+groups = res.json() if res.status_code == 200 else []
+
+for g in groups:
+    if st.button(g["name"], key=g["_id"]):
+        st.session_state.group_id = g["_id"]
+        st.rerun()
